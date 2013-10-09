6c1ec0c0c470c684582a4c9b1b63bb9436828733
msmummy <msmummy@gmail.com>
2013-10-09 15:04:50 -0400

    configs

--- script/create_node_boto.py
+++ script/create_node_boto.py
@@ -132,7 +132,7 @@ if __name__ == "__main__":
     #    create_instance(conn, ami, key)
         show_instance(conn)
 
-        terminal_all(conn)
-    #print_hosts()
-    #print_config()
-    #print_jpaxos_config()
+    #    terminal_all(conn)
+    print_hosts()
+    print_config()
+    print_jpaxos_config()
